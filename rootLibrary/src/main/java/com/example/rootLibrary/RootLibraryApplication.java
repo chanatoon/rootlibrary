31c9ed6ee30c104792b69f95ae001cd679b5c922
akimoto <akimoto@example.com>
2026-06-18 08:44:01 +0900

    プロジェクト追加

---
+++ rootLibrary/src/main/java/com/example/rootLibrary/RootLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.rootLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RootLibraryApplication.class, args);
+	}
+
+}
---
+++ rootLibrary/src/main/java/com/example/rootLibrary/controller/Login.java
@@ -0,0 +1,13 @@
+package com.example.rootLibrary.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Login {
+	
+	public void loging() {
+		
+		return "";
+	}
+
+}
